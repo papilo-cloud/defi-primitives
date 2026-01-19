e6d62925efa1d19c8f2be84c19c518e51183112e
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2026-01-19 15:00:02 +0100

    add import path

--- delegateCall/use-cases/proxy-pattern/minimal-proxy/Example1.sol
+++ delegateCall/use-cases/proxy-pattern/minimal-proxy/Example1.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import {MinimalProxy} from "./MinimalProxy.sol";
+
 /**
  * @title Simple Token Cloning
  * @notice Step 1: Create Implementation
