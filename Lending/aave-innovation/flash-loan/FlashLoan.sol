7fdfcb0e8d5c38057b810d738d6b75ba4d5ec9f3
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2026-02-02 03:45:50 +0100

    feat: flash loan

--- Lending/aave-innovation/flash-loan/FlashLoan.sol
+++ Lending/aave-innovation/flash-loan/FlashLoan.sol
@@ -5,6 +5,7 @@ interface IERC20 {
     function balanceOf(address account) external view returns (uint256);
     function transfer(address to, uint256 amount) external returns (bool);
     function transferFrom(address from, address to, uint256 amount) external returns (bool);
+    function approve(address spender, uint256 amount) external returns (bool);
 
 }
 
